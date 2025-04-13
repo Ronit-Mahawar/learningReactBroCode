1bf101aa305bc76968341e9c68999d354e142cd8
Ronit Mahawar <ronitmahawar14@gmail.com>
2025-04-13 19:11:41 +0530

    first commit

---
+++ my-react-app/src/Food.jsx
@@ -0,0 +1,14 @@
+function Food(){
+    const food1="apple";
+    const food2="orange";
+    const food3="banana";
+
+return(
+    <ol>
+     <li>{food1}</li>
+     <li>{food2}</li>
+     <li>{food3}</li>
+    </ol>
+);
+}
+export default Food
---
+++ my-react-app/src/Header.jsx
@@ -0,0 +1,20 @@
+function Header(){
+   return(
+      <header>
+        <h1>My Website</h1>
+        <nav>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About Us</a></li>
+                <li><a href="#">Contact Us</a></li>
+                <li><a href="#">terms and condition</a></li>
+                <hr />
+                
+            </ul>
+        </nav>
+   
+      </header>
+   ); 
+      
+}
+export default Header ;
