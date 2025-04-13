712c50d43a333002f0423d67cb4e862ae32c9d5d
Ronit Mahawar <ronitmahawar14@gmail.com>
2025-04-14 02:42:30 +0530

    BUTTON

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,6 +1,7 @@
 import Header from "./Header"
 import Footer from "./Footer"
 import Food from "./Food" 
+import Button from "./Button"
 /hello/
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <Header/>
     <Food/>
     <Footer/>
+    <Button/>
    
   </>
 
